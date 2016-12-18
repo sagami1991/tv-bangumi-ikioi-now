2c0a777feb3bb54e94d8c70f59e62689177c2920
yasuaki <yasuaki@test>
2016-12-18 18:05:15 +0900

    no message

--- client/src/ts/app.ts
+++ client/src/ts/app.ts
@@ -10,26 +10,29 @@ import dateFormat = require('dateformat');
 class MainApp {
     private static HTML = Handlebars.compile(`
     <table class="table">
-        <tr>
-            <th>id</th>
-            <th>開始時刻</th>
-            <th>終了時刻</th>
-            <th>タイトル</th>
-            <th>コメント数</th>
-        </tr>
-            {{#each bangumiList}}
-                <tr>
-                    <td>
-                        <img src="/resource/channel/{{id}}.png"></img>
-                    </td>
-                    <td>{{dateformat beginDate "HH:mm"}}</td>
-                    <td>{{endDate}}</td>
-                    <td>{{title}}</td>
-                    <td>{{commentCount}}</td>
-                </tr>
-            {{/each}}
+		<thead>
+			<tr>
+				<th></th>
+				<th>開始</th>
+				<th>終了</th>
+				<th width="300">タイトル</th>
+				<th class="center">コメント</th>
+			</tr>
+		</thead>
+		<tbody>
+			{{#each bangumiList}}
+				<tr style="{{rowColor commentCount}}">
+					<td class="img">
+						<img src="/resource/channel/{{id}}.png"></img>
+					</td>
+					<td>{{dateformat beginDate "HH:mm"}}</td>
+					<td>{{dateformat endDate "HH:mm"}}</td>
+					<td>{{title}}</td>
+					<td class="comment-count">{{commentCount}}</td>
+				</tr>
+			{{/each}}
+		</tbody>
     </table>
-
     `);
 
     public static init() {
@@ -40,6 +43,10 @@ class MainApp {
 
     private setHandlebarsHelper() {
         Handlebars.registerHelper("dateformat", (date: Date, pattern: string) => dateFormat(date, pattern) );
+        Handlebars.registerHelper("rowColor", (comment: number) => {
+			const gb = 255 - Math.min(Math.ceil(comment / 400 * 255), 255);
+			return `background-color: rgba(255, ${gb}, ${gb}, 0.3);`;
+		});
     }
 
     /**
@@ -56,7 +63,6 @@ class MainApp {
             dataJson.sort((before , after) => before.id - after.id);
             const element = document.querySelector('.table-wrapper');
             element.innerHTML = MainApp.HTML({bangumiList: dataJson});
-            console.log(dataJson);
         }
     }
 }
--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require('gulp');
 var ts = require("gulp-typescript");
 var tsProject = ts.createProject('tsconfig.json', {typescript: require('typescript')});
 var sourcemaps = require('gulp-sourcemaps');
+const spawn = require('child_process').spawn;
+var node;
 
 gulp.task('ts', function () {
 	return gulp.src(['./server/**/*.ts', '!./server/**/*.spec.ts'])
@@ -13,8 +15,16 @@ gulp.task('ts', function () {
 
 
 gulp.task('watch', function() {
-	gulp.start(["ts"]);
-	gulp.watch(['./server/**/*.ts'], ['ts']);
+	gulp.start(["ts", "server"]);
+	gulp.watch(['./server/**/*.ts'], ['ts', "server"]);
 });
+
+gulp.task('server',["ts"], function() {
+  if (node) {
+	  node.kill()
+  }
+  node = spawn('node', ['./server/server.js'], {stdio: 'inherit'})
+});
+
 
 gulp.task('default', ["ts"]);
